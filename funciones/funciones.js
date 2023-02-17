8f3c12c6cfb1e493f2593d85f469fd55131737a5
AdrianC89 <107586138+AdrianC89@users.noreply.github.com>
2023-02-17 17:45:41 -0300

    Add files via upload
    nuevos archivos subidos

---
+++ funciones/funciones.js
@@ -0,0 +1,14 @@
+function fijar(n) {
+    document.getElementById("subseccion"+n).style.display="block"
+    }
+
+function ver(n) {
+         document.getElementById("lenguaje"+n).style.display="block"
+         }
+         
+function ocultar(n) {
+         document.getElementById("lenguaje"+n).style.display="none"
+         }
+
+
+
